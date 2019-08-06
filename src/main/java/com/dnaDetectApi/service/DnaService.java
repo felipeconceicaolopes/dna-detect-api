ded1969d07e5bb696530340318cb1e533954b6bc
ftiburcio <felipeconceicaolopes@gmail.com>
2019-08-06 04:12:24 -0300

    Transforming array into two-dimensional array and validate vertically and horizontally if DNA is simian

--- src/main/java/com/dnaDetectApi/service/DnaService.java
+++ src/main/java/com/dnaDetectApi/service/DnaService.java
@@ -10,7 +10,18 @@ import org.springframework.stereotype.Service;
 public class DnaService {
 	
 	public Boolean isSimian(String[] dna) {
-		return hasNitrogenBase(dna);
+		
+		if(hasNitrogenBase(dna)) {
+			char[][] matrixDna = arraySequencesToMatrix(dna);
+			
+			if(validatingHorizontallyIfSimian(matrixDna))
+				return Boolean.TRUE;
+			
+			if(validatingVerticallyIfSimian(matrixDna))
+				return Boolean.TRUE;
+		}
+		
+		return Boolean.FALSE;
 	}
 	
 	private Boolean hasNitrogenBase(String[] dna) {
@@ -29,5 +40,72 @@ public class DnaService {
 		}
 		
 		return Boolean.TRUE;
+	}
+
+	private static char[][] arraySequencesToMatrix(String[] dna) {
+		StringBuilder baseM = new StringBuilder();
+		char[][] m = new char[6][6];
+		int count = 0;
+		
+		for(String base : dna) {
+			baseM.append(base);
+		}
+		
+		for(int i = 0; i < dna.length; i++) {
+			for(int z = 0; z < dna.length; z++) {
+				m[i][z] = baseM.charAt(count);
+				count++;
+			}
+		}
+		
+		return m;
+	}
+	
+	private static Boolean validatingHorizontallyIfSimian(char dna[][]) {
+
+		for(int i = 0; i < dna.length; i++) {
+			int count = 0;
+			StringBuilder teste = new StringBuilder();
+			for(int z = 0; z < dna.length - 1; z++) {
+				int x = z + 1;
+				int y = 0;
+				if(z > dna.length)
+					y = z - 1;
+				
+				if(dna[i][z] == dna[i][x] && dna[i][y] == dna[i][x]) {
+					count++;
+					teste.append(dna[i][z]);
+				}
+			}
+			if(count >= 3) {	
+				return Boolean.TRUE;
+			}
+		}
+		
+		return Boolean.FALSE;
+	}
+	
+	private static Boolean validatingVerticallyIfSimian(char dna[][]) {
+		
+		for(int z = 0; z < dna.length; z++) {
+			int count = 0;
+			StringBuilder teste = new StringBuilder();
+			for(int i = 0; i < dna.length - 1; i++) {
+				int x = i + 1;
+				int y = 0;
+				if(i > dna.length)
+					y = i - 1;
+				
+				if(dna[i][z] == dna[x][z] && dna[y][z] == dna[x][z]) {
+					count++;
+					teste.append(dna[i][z]);
+				}
+			}
+			if(count >= 3) {
+				return Boolean.TRUE;
+			}			
+		}
+		
+		return Boolean.FALSE;
 	}
 }
