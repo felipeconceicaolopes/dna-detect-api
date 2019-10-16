8199d4e8b42961c62e9828150255f2601053bbf6
ftiburcio <felipeconceicaolopes@gmail.com>
2019-10-16 00:39:43 -0300

    Adding local and dev properties and updating properties for prod

--- src/test/java/com/dnaDetect/dna/DnaApplicationTests.java
+++ src/test/java/com/dnaDetect/dna/DnaApplicationTests.java
@@ -2,12 +2,13 @@ package com.dnaDetect.dna;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.dnaDetectApi.DnaDetectApiApplication;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(classes = DnaDetectApiApplication.class) 
 public class DnaApplicationTests {
 
